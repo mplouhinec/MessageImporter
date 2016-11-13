36d45a25129431a80a956230184b0a45cf06fb96
Mikaël PLOUHINEC <Mikal PLOUHINEC>
2016-11-13 14:50:31 +0100

    Add Message class and Project file

---
+++ MessageImporter/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageImporter
+{
+    /// <summary>
+    /// Base Class for a message
+    /// </summary>
+    public class Message
+    {
+        #region Properties
+
+        public DateTime Date { get; set; }
+
+        public string Recipient { get; set; }
+
+        public string Sender { get; set; }
+
+        public string Content { get; set; }
+
+        #endregion
+    }
+}
